36ff28aa23f59cffc0e67a723df502081ac21e42
Мария Петрова <petrich0Orus@gmail.com>
2023-05-04 00:07:38 +0300

    Fixing raise near NotImplementedError.

--- homework.py
+++ homework.py
@@ -44,7 +44,7 @@ class Training:
 
     def get_spent_calories(self) -> float:
         """Получить количество затраченных калорий."""
-        NotImplementedError
+        raise NotImplementedError
 
     def show_training_info(self) -> InfoMessage:
         """Вернуть информационное сообщение о выполненной тренировке."""
